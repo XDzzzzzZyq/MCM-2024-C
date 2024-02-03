eb654c20d65150b3bdb9f5e2c87a66dba42c2794
XDzZyq <xiaodouzizyq@gmail.com>
2024-02-03 14:56:09 +0800

    2.3 new plots

--- utils/database.py
+++ utils/database.py
@@ -13,6 +13,31 @@ def select_match(data: pd.DataFrame, player1: str, player2: str) -> [pd.DataFram
 
     return eg1, win_series
 
+def select_match_norm(data: pd.DataFrame, player1: str, player2: str) -> [pd.DataFrame, pd.DataFrame]:
+    
+    eg1 = data[(data["player1"] == player1) & (data["player2"] == player2)]
+    eg1['elapsed_time'] = pd.to_datetime(eg1['elapsed_time'], format='%H:%M:%S')
+    eg1['p1_win'] = eg1['point_victor'] == 1
+    eg1['p2_win'] = eg1['point_victor'] == 2
+    eg1['p2_game_win'] = eg1['game_victor'] == 2
+    eg1['p1_game_win'] = eg1['game_victor'] == 1
+
+    return eg1.sort_values(by='elapsed_time').set_index('elapsed_time')
+
+
+def get_set_range(match: pd.DataFrame) -> list[pd.Timestamp]:
+
+    match = match.copy()
+    match["new set"] = match["set_no"] != match["set_no"].shift(1)
+    new_set = []
+
+    for _index, _row in match.iterrows():
+        if _row["new set"]:
+            new_set += [_index]
+    new_set.append(max(match.index))
+
+    return new_set
+
 
 def get_velocity(P: pd.Series, I1: float, I2: float) -> [pd.Series, pd.Series]:
     I1I2 = I1*I2
@@ -34,23 +59,26 @@ def get_velocity_2(P: pd.Series, I1: float, I2: float) -> [pd.Series, pd.Series]
     dom = -I1I2+I1**2*P+I1I2P
 
     w1 = (I1*P+SQRT)/dom
-    w2 = 1-I1**2*P/dom - I1*SQRT/dom
-    w2 /= I2
+    w2 = (1-w1)/I2
 
     return w1, w2
 
 
-def _expected_P(P: pd.Series, l:float = 0.8) -> float:
+def _expected_P(P: pd.Series, l:float = 0.2) -> float:
     w = len(P)
     ker = l*np.power(1-l, np.arange(0, w))
     ker[-1] += (1-l)**(w)
     #print(ker)
     #print(sum(ker))
-    return sum(P*ker)
+    return sum(P*ker[::-1])
 
 
-def expected_P(P: pd.Series, l:float = 0.8, w:int = 15) -> pd.Series:
+def expected_P(P: pd.Series, l:float = 0.2, w:int = 15) -> pd.Series:
     return pd.Series([_expected_P(P[max(0, i-w+1):i+1], l) for i in range(len(P))])
+
+
+def to_minute(time: pd.DatetimeIndex) -> float:
+    return time.hour*60 + time.minute + time.second/60
 
 
 if __name__ == '__main__':
@@ -59,3 +87,11 @@ if __name__ == '__main__':
     print(len(expected_P([1,2,3,4,5,6,7,8,9,10], 0.8, 5)))
     expected_P([1,2,3,4,5,6,7,8,9], 0.2, 5)
 
+    dictionary = pd.read_csv("../data_dictionary.csv")
+    data = pd.read_csv("../Wimbledon_featured_matches.csv")
+    data = select_match_norm(data, "Carlos Alcaraz", "Nicolas Jarry")
+    exp = expected_P(data["p1_distance_run"])
+    print(exp.head())
+
+    set_range = get_set_range(data)
+    print(set_range)
--- utils/visualize.py
+++ utils/visualize.py
@@ -1,9 +1,17 @@
 import matplotlib.pyplot as plt
+import pandas as pd
+
+def draw_range(new_set: list[pd.Timestamp]):
+    for i in range(len(new_set)):
+        if i%2==0 and i+1<len(new_set):
+            plt.axvspan(new_set[i], new_set[i+1], color='orange', alpha=0.3, label=f'Interval {i}')
+
 
 def set_label(title, xlabel, ylabel):
     plt.title(title, fontsize=20)
     plt.xlabel(xlabel, fontsize=15)
     plt.ylabel(ylabel, fontsize=15)
+
 
 def save(name):
     plt.savefig(f'figs/{name}.eps', format='eps', bbox_inches='tight')
